36b248d96260b5342e61a02f78fc4055d586b2ee
Michael <soplomiha@gmail.com>
2022-03-23 22:46:53 +0300

    Add model Owner

--- property/admin.py
+++ property/admin.py
@@ -1,18 +1,22 @@
 from django.contrib import admin
 
-from .models import Flat, Complaint
+from .models import Flat, Complaint, Owner
 
 
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner', ]
     readonly_fields = ('created_at',)
-    list_display = ('address', 'owners_phonenumber', 'owner_pure_phone', 'price', 'new_building',
+    list_display = ('address', 'price', 'new_building',
                     'construction_year', 'town', )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     raw_id_fields = ('liked_by',)
 
+
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
--- property/models.py
+++ property/models.py
@@ -6,7 +6,8 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Flat(models.Model):
     owner = models.CharField('ФИО владельца', max_length=200)
-    owner_pure_phone = PhoneNumberField('Нормализованный номер владельца', blank=True)
+    owner_pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца', blank=True)
     owners_phonenumber = models.CharField('Номер владельца', max_length=20)
     new_building = models.NullBooleanField()
     created_at = models.DateTimeField(
@@ -55,6 +56,15 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Owner(models.Model):
+    full_name = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца', blank=True)
+    flats = models.ManyToManyField(
+        Flat, related_name='owners', verbose_name='Квартиры в собственности')
 
 
 class Complaint(models.Model):
