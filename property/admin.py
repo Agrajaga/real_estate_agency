6997b61d6f675dd3ec152538c246b15608f882aa
Michael <soplomiha@gmail.com>
2022-03-24 00:08:20 +0300

    Add inline owners

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class OwnersInline(admin.TabularInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner', ]
@@ -12,12 +17,15 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
     raw_id_fields = ('liked_by',)
+    inlines = [
+        OwnersInline,
+    ]
 
 
 @admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
-    
+
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
