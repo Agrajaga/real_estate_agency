3ace4aa14474fa77a51f0ffb0f7475b12518615f
Michael <soplomiha@gmail.com>
2022-03-28 22:05:32 +0300

    Fix owner model

--- property/models.py
+++ property/models.py
@@ -56,12 +56,14 @@ class Flat(models.Model):
 
 
 class Owner(models.Model):
-    full_name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owners_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
+    full_name = models.CharField(
+        'ФИО владельца', max_length=200, db_index=True)
+    owners_phonenumber = models.CharField(
+        'Номер владельца', max_length=20, db_index=True)
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца', blank=True, db_index=True)
     flats = models.ManyToManyField(
-        Flat, related_name='owners', verbose_name='Квартиры в собственности')
+        Flat, related_name='owners', verbose_name='Квартиры в собственности', blank=True)
 
     def __str__(self) -> str:
         return self.full_name
